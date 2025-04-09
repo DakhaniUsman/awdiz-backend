d0e348c70dd9e074efa0fa4de7104df2c9253eb9
AvinashT31 <115381340+DakhaniUsman@users.noreply.github.com>
2025-04-09 18:44:44 +0530

    BACKEND

--- index.js
+++ index.js
@@ -4,14 +4,33 @@ console.log("Hello from Index");
 
 // here we are going to create express server
 
+const books = [{ id: 1, name: "Death Note", author: "Light Yagami" }];
+
 const app = express();
 
 app.get("/", (req, res) => {
-  return res.send("Welcome to Express Server 0_0 ");
+   res.send("Welcome to Express Server 0_0 ");
+});
+app.get("/books", (req,res)=>{
+   res.send(`${books.map((book)=>(
+    `${book.id}. ${book.name} by ${book.author}`
+  ))}`)
+});
+
+app.get("/products", (req,res)=>{
+  res.send("My Products...")
+})
+
+app.get("/greet", (req,res)=>{
+  res.send("Welcome!")
+})
+
+app.use((req, res) => {
+  res.status(404).send("OOPS Looks like you are lost >_<");
 });
 
 const backendPort = 8000;
 
 app.listen(backendPort, () => {
-  console.log("Server is running on port 8000");
+  console.log(`Server is running on port ${backendPort}`);
 });
